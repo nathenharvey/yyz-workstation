06fc815c7808374bc9c312666c1133e6a5e9e80e
Nathen Harvey <nharvey@chef.io>
2015-08-05 19:02:08 +0000

    Workstation 2.0
    Add more details to the MOTD

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -2,6 +2,10 @@ package 'vim' do
   action :install
 end
 
+package 'emacs' do
+  action :install
+end
+
 package 'tree' do
   action :install
 end
@@ -11,7 +15,18 @@ package 'git' do
 end
 
 file '/etc/motd' do
-  content "This server is property of Chef\n"
+  content "This server is property of Chef
+
+  IPADDRESS:  #{node["ipaddress"]}
+  HOSTNAME:   #{node["hostname"]}
+  MEMORY:     #{node["memory"]["total"]}
+  CPU:        #{node["cpu"]["0"]["mhz"]}
+"
   manage_symlink_source true
 end
+
+
+
+
+
 
