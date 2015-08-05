d8dd3d68e567171c6ea482e8ec2383e1fdb22634
Nathen Harvey <nharvey@chef.io>
2015-08-05 19:01:42 +0000

    Write an integration test

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,32 @@ require 'spec_helper'
 describe 'workstation::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  #
+  it 'creates an motd file' do
+    expect(file '/etc/motd').to exist
+  end
+  
+  it 'creates an motd file with the correct content' do
+    expect(file('/etc/motd').content).to contain /property/i
+  end
+  
+  # describe file('/etc/passwd') do
+  #   it { should exist }
+  # end
+
+  it 'installs tree' do
+    expect(package 'tree').to be_installed
+  end
+
+  it 'installs git' do
+    expect(package 'git').to be_installed
+  end
+
+  it 'installs vim' do
+    expect(package 'vim-enhanced').to be_installed
+  end
+  
+  it 'installs emacs' do
+    expect(package 'emacs').to be_installed
   end
 end
